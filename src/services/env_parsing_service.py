ec86a3aefd104dce0cd892f8afbcfbd018dc7b74
Mainakdey1 <mainakde02@gmail.com>
2026-08-29 05:16:13 +0530

    Fixed an edge case: non existent env falsely claimed as existing

--- src/services/env_parsing_service.py
+++ src/services/env_parsing_service.py
@@ -4,9 +4,10 @@ from services.local_environment_parsing_and_write_service import local_env_parsi
 
 def env_parsing_service(local_env_data: Dict[str, str], recieved_env_data: Dict[str, str]):
     differ = False
-    for key, value in recieved_env_data.items():
-        if key not in local_env_data or local_env_data[key] != value:
-            differ = True
+    if len(local_env_data) != 0:
+        for key, value in recieved_env_data.items():
+            if key not in local_env_data or local_env_data[key] != value:
+                differ = True
 
     if differ:
         print('The environment variables in the backend differ from you local .env file.')
@@ -16,6 +17,14 @@ def env_parsing_service(local_env_data: Dict[str, str], recieved_env_data: Dict[
             local_env_parsing_and_write_service(env_data=recieved_env_data)
         else:
             print('Keeping local .env file unchanged.')
+    elif len(local_env_data) == 0:
+        print('A local .env file does not exist in this repository but a remote env has been found. Do you wish to populate this repo with it? (y/n)')
+        choice = input().lower()
+        if choice == 'y':
+            local_env_parsing_and_write_service(env_data=recieved_env_data)
+        else:
+            print('Making no changes.')
+
     else:
         print('The environment variables in the backend are the same as your local .env file. No changes needed.')
                                                 
